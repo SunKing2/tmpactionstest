14c094e6c5b4338f9740c43e39d056ce5c42c60e
SunKing2 <louievbl4t@gmail.com>
2023-07-23 19:44:50 -0400

    added adder and a 2 tests in one test_adder.py for add_me

---
+++ adder.py
@@ -0,0 +1,13 @@
+# spew hello world
+def add_me(a, b):
+    return a + b
+
+
+def do_main():
+    resulte = add_me(7, 2)
+    print(resulte)
+
+
+# dunder
+if __name__ == '__main__':
+    do_main()
--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-# spew hello world
-print("Hello finer yet  ooh World!")
+from adder import add_me
+
+
+def do_main():
+    result = add_me(7, 2)
+    # spew short message and a addition result
+    print('Hello World! The result of 7 + 2 is: ', result)
+
+
+# dunder
+if __name__ == '__main__':
+    do_main()
