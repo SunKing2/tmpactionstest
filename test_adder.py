bba4bc9e4d13dc3d014698f3d7a2d774c3629899
SunKing2 <louievbl4t@gmail.com>
2023-07-23 19:59:21 -0400

    added adder and a 2 tests in one test_adder.py for add_me

--- test_adder.py
+++ test_adder.py
@@ -1,17 +1,13 @@
 import unittest
-
-# we are gonna test add_me from adder.py
 from adder import add_me
 
 
 class MyTestCase(unittest.TestCase):
     def test_add_me_positive_arguments(self):
-        # we are gonna test add_me from adder.py
         self.assertEqual(9, add_me(7, 2))
 
     def test_add_me_negative_arguments(self):
-        # we are gonna test add_me from adder.py
-        self.assertEqual(-5, add_me(-2, -3))
+        self.assertEqual(-999999, add_me(-2, -3))
 
 
 if __name__ == '__main__':
